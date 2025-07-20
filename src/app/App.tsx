18f4b37873157fe214ec4b41eaa9c478ff3b41ee
AntonZaika <lycan44444@gmail.com>
2025-07-20 17:47:35 +0200

    feat(React-Router): создан конфиг для роутинга по приложению
    1 - Написан routeConfig для более удобного добавления роутов, страниц

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import classNames from "classnames";
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import AppRouter from "app/providers/router/ui/AppRouter";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -15,12 +12,7 @@ const App = () => {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О нас</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
