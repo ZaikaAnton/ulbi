eabda3d3fec87d4c485d42b395086bd3ad94938a
AntonZaika <lycan44444@gmail.com>
2025-07-28 18:53:29 +0200

    feat(UI): добавление widget Sidebar
    1 - Написан виджет Sidebar 2 - ThemeSwitcher перенесен в Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import classNames from "classnames";
 import cls from "./Navbar.module.scss";
 import { AppLink } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
@@ -10,7 +9,6 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, [className])}>
-      <ThemeSwitcher />
       <div className={classNames(cls.links)}>
         <AppLink
           to="/"
---
+++ src/widgets/SideBar/ui/SideBar/Sidebar.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+import cls from "./Sidebar.module.scss";
+import { useState } from "react";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={classNames(cls.sideBar, className, {
+        [cls.collapsed]: collapsed,
+      })}
+    >
+      <button onClick={onToggle}>toggle</button>
+      <div className={classNames(cls.switchers)}>
+        <ThemeSwitcher />
+        {/* LanguageSwitcher */}
+      </div>
+    </div>
+  );
+};
